bd992c0b5132da45919094c8fe7a9720785dde80
mizzul <izzulhaq605@gmail.com>
2021-09-20 00:27:25 +0700

    menambahkan Contoh1.php

---
+++ Latihan-WP2/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Izzul Haq
+            Saya tinggal di daerah Cakung
+            Saya berkuliah di Universitas Bina Sarana Informatika
+            dengan prodi Sistem Informasi";
+    }
+}
