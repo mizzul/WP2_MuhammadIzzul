e31ae175dbd0c655c9b98584197a846dac6096b6
mizzul <izzulhaq605@gmail.com>
2021-09-26 08:48:59 +0700

    menambahkan Contoh1

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Izzul Haq
+            Saya tinggal di daerah Cakung
+            Hobby saya badminton";
+    }
+}
