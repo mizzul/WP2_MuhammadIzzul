c6d517df2577b1a4d08d9ec0b3fe072c734d9258
mizzul <izzulhaq605@gmail.com>
2021-09-27 00:52:00 +0700

    menambahkan Latihan1.php,model_latihan1.php,view-latihan.php

--- Latihan-WP2/application/controllers/Contoh1.php
+++ Latihan-WP2/application/controllers/Contoh1.php
@@ -6,7 +6,6 @@ class Contoh1 extends CI_Controller
         echo "<h1>Perkenalkan</h1>";
         echo"Nama saya Muhammad Izzul Haq
             Saya tinggal di daerah Cakung
-            Saya berkuliah di Universitas Bina Sarana Informatika
-            dengan prodi Sistem Informasi";
+            Hobby saya badminton";
     }
 }
